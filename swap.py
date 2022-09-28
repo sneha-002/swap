71d1115052aa4c300c4fc7026a2c3e361dfae2eb
rohithaswathi <60994428+rohithaswathi@users.noreply.github.com>
2022-09-28 22:14:49 +0530

    without third var

--- swap.py
+++ swap.py
@@ -2,9 +2,13 @@ x = input()
 y = input()
 print('The value of x is {}'.format(x))
 print('The value of y is {}'.format(y))
-print('edited by sneha')
 temp_var = x
 x = y
 y = temp_var
 print('The value of x after swapping is {}'.format(x))
 print('The value of y after swapping is {}'.format(y))
+x=x+y
+y=x-y
+x=x-y
+print(x)
+print(y)
