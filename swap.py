c10a0f003edab98b726b68e25237cc98d01ad285
sneha-002 <61006847+sneha-002@users.noreply.github.com>
2022-09-28 22:28:46 +0530

    edited

--- swap.py
+++ swap.py
@@ -5,6 +5,7 @@ print('The value of y is {}'.format(y))
 temp_var = x
 x = y
 y = temp_var
+print('edited by sneha')
 print('The value of x after swapping is {}'.format(x))
 print('The value of y after swapping is {}'.format(y))
 x=x+y
